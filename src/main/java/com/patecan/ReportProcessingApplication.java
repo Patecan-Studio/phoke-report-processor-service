7462060ab9f1aa41de85e0fec05c24fb81e2d68b
Trần Thiên Trọng <tranthientrong.it@gmail.com>
2024-09-17 21:35:23 +0700

    init

--- src/main/java/com/patecan/ReportProcessingApplication.java
+++ src/main/java/com/patecan/ReportProcessingApplication.java
@@ -14,6 +14,7 @@ import java.util.function.Function;
 @SpringBootApplication
 public class ReportProcessingApplication implements CommandLineRunner {
 
+
     @Autowired
     private MainProcessor mainProcessor;
 
